29e0119666c831101dde5d4390e189fe84c1858b
Gareth Flynn <gareth.t.flynn@gmail.com>
2022-12-05 14:43:50 -0500

    fixed typo in client/utils

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-serverexpress')
-const path = require('path');
-
+const { ApolloServer } = require('apollo-server-express')
+const path = require('path')
+const {authMiddleware} = require('./utils/auth')
 const {typeDefs, resolvers} = require('./schemas')
 const db = require('./config/connection');
-// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,6 +13,8 @@ const server = new ApolloServer({
   context: authMiddleware
 })
 
+
+server.applyMiddleware({ app })
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -22,8 +23,21 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+  
+// Call the async function to start the server
+  startApolloServer(typeDefs, resolvers);
