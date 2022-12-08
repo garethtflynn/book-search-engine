6d05466f66bdf4c7811a4f3a5011304c68dc47a5
Gareth Flynn <gareth.t.flynn@gmail.com>
2022-12-08 18:03:26 -0500

    deploying once again

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+// connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bookDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
