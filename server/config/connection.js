3e0f3f57a1136bc8e5a6eb142af2329dfa05c021
Gareth Flynn <gareth.t.flynn@gmail.com>
2022-12-12 18:01:58 -0500

    commiting to heroku to try and fix error

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// connection 
+// connection to mongo
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bookDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
